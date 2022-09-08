7a7641b837187d2841ac0e9c75a382ae1793f093
ALAREEF <alareefadegbite@gmail.com>
2022-09-08 06:18:51 -0700

    My print task commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,10 @@
 #nclude <stdio.h>
-int main()
+/**
+ * main - Entry point 
+ *Return: Always 0 (Success)
+ */
+int main(void)
 {
-  printf( "with proper grammar, but the outcome is a piece of art\n" ) ;
-  return ( 0 ) ;
+  puts("\"with proper grammar, but the outcome is a piece of art");
+  return (0);
 }
