7c9dc0996b2893bb51916ecbcac4888a4cff11be
ALAREEF <alareefadegbite@gmail.com>
2022-09-13 05:43:25 -0700

    fix 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+int main(void)
+{
+//declare a variable
+char alphabet;
+for (alphabet = 'a';alphabet <='Z';alphabet++)
+{
+// print the value of the variable
+putchar(alphabet);
+}
+putchar("\n");
+return (0);
+
