75422f3e922b2ec6e018d289586facc82d5d2faa
ALAREEF <alareefadegbite@gmail.com>
2022-09-13 14:57:44 -0700

    fix put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,4 @@
-#!/bin/bash
-#include "holberton.h"
-/**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
-{
-char *sh = "Holberton";
-while (*sh)
-{
-_putchar(*sh);
-sh++;
-}
-_putchar('\n');
+#include "main.h"
+int main()
+putchar("_putchar\n")
 return (0);
-}
