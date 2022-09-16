5e02621cd269587fc66d20910b1bd8ce3fcc2e33
ALAREEF <alareefadegbite@gmail.com>
2022-09-16 03:38:17 -0700

    fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-	char alphabet;
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-	{
-		putchar(alphabet);
-	}
-	{
-		char alphabet;
-				for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
-					putchar(alphabet);
-	}
-		putchar('\n');
-		return (0);
+char alphabet;
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+char alphabet;
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
 }
