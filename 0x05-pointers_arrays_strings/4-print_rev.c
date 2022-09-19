582cfd6c57f9d7bc3831ba4254afe5a367797208
ALAREEF <alareefadegbite@gmail.com>
2022-09-19 07:01:09 -0700

    fix print

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,11 +8,11 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
-	while (s[i] != '\0')
-		i++;
-			while (i)
-				_putchar(s[--i]);
-			_putchar('\n');
-		}
+int i = 0;
+while (s[i] != '\0')
+i++;
+while (i)
+_putchar(s[--i]);
+_putchar('\n');
+}
 
