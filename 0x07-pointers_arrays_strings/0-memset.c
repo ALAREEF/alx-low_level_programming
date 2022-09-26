9f2dee02458f1293035addf5c81f5537c0edeb75
ALAREEF <alareefadegbite@gmail.com>
2022-09-26 09:25:19 -0700

    fix set

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,12 +8,11 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	char  *start = s;
-  while (n--)
-  {
-	  *s = b;
-	  s++;
-  }
-  return (start);
-
+char  *start = s;
+while (n--)
+ {
+*s = b; 
+s++;
+}
+return (start);
 }
